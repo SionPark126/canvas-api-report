e7a823d9058c298ec38f0da773b8e603976aa2de
etmParks <40302253+etmParks@users.noreply.github.com>
2018-10-04 10:43:30 -0700

    upload index and function files

---
+++ funcions.js
@@ -0,0 +1,15 @@
+const Http = new XMLHttpRequest();
+const url='https://canvas.instructure.com/api/v1/courses/';
+Http.open("GET", url);
+Http.send();
+Http.setReqestHeader({
+  "Content-Type": "application/json",
+	"Authorization": "6211~UhALRSFjCu1Do3ZfMFKIziIONkxD6XDG87TBvkWkPjonud7SIu8037RX9f3d5UIO"
+
+})
+Http.onreadystatechange = function ReceivedCallback(e)=>{
+  if ( this.readyState == 4 & this.status ==200){
+    console.log(Http.responseText);
+  }
+
+}
