9f4ed0661c8c5fa195389c43f08db394a9f088e5
etmParks <40302253+etmParks@users.noreply.github.com>
2018-10-04 15:30:39 -0700

    course numbers and course names can be grabbed

--- functions.js
+++ functions.js
@@ -1,15 +1,34 @@
-const Http = new XMLHttpRequest();
-const url='https://canvas.instructure.com/api/v1/courses/';
-Http.open("GET", url);
-Http.send();
-Http.setReqestHeader({
-  "Content-Type": "application/json",
-	"Authorization": "6211~UhALRSFjCu1Do3ZfMFKIziIONkxD6XDG87TBvkWkPjonud7SIu8037RX9f3d5UIO"
+// const Http = new XMLHttpRequest();
+// const url='https://canvas.instructure.com/api/v1/courses/';
+// Http.open("GET", url);
+// Http.send();
+// Http.setReqestHeader({
+//   "Content-Type": "application/json",
+// 	"Authorization": "6211~UhALRSFjCu1Do3ZfMFKIziIONkxD6XDG87TBvkWkPjonud7SIu8037RX9f3d5UIO"
+//
+// })
+// Http.onreadystatechange = function ReceivedCallback(e){
+//   if ( this.readyState == 4 & this.status ==200){
+//     console.log(Http.responseText);
+//   }
+//
+// }
+var data = canvasInfo;
+console.log(data)
+function submit(){
 
-})
-Http.onreadystatechange = function ReceivedCallback(e){
-  if ( this.readyState == 4 & this.status ==200){
-    console.log(Http.responseText);
+  $("#result").text(" ");
+  var variable = $("#options")[0].value;
+  console.log(variable)
+  if (variable =="courseNum"){
+    $("#result").text(data.length);
   }
+  else if(variable == "names"){
+    var text = "";
+    for (var i =0; i <data.length ;i++){
+      text += data[i].name + '\n' ;
+    }
 
+    $("#result").text(text);
+  }
 }
